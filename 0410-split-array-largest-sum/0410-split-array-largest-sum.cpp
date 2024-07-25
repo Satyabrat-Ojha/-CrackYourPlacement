491e8ad1e01aaea74256c9c49c72aaa2ab9adcad
Satyabrat Ojha <satyabratojha04@gmail.com>
2024-07-26 00:54:55 +0530

    Time: 0 ms (100%), Space: 9.5 MB (16.57%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int m) {
+        long long l = *max_element(nums.begin(),nums.end());
+        long long r = accumulate(nums.begin(), nums.end(), 0LL);
+        while(l < r) {
+            long long mid = l + (r - l) / 2;
+            if(duable(mid, m, nums)) r = mid;
+            else l = mid + 1;
+        }
+        return l;
+    }
+
+    bool duable(long long mx, int m, vector<int>& nums) {
+        long long sum = 0;
+        for(long long num : nums) {
+            sum += num;
+            if(sum > mx) {
+                sum = num;
+                m--;
+            }
+        }
+        return m > 0;
+    }
+};
