a201f4ce38c6b69515ce0a7ccc59b8cc96da3daf
Satyabrat Ojha <satyabratojha04@gmail.com>
2024-10-03 15:28:10 +0530

    Time: 23 ms (94.24%), Space: 21.7 MB (46.34%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1, high = 1e9;
+        while(low < high) {
+            int mid = low + (high - low) / 2;
+            if(check(mid, h, piles)) high = mid;
+            else low = mid + 1;
+        }
+        return high;
+    }
+
+    bool check(int s, int h, vector<int> &piles) {
+        int ans = 0;
+        for(auto pile:piles) {
+            ans += pile/s;
+            if(pile%s) ans++;
+            if(ans > h) return 0;
+        }
+        return 1;
+    }
+};
